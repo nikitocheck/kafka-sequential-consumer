7da81c51d55858b0b546071f4b67455916a44bcf
tailwhiper <tailwhiper@gmail.com>
2021-08-16 23:18:30 +0300

    clean up warnings

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -7,12 +7,10 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitLast
-import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
 import org.testcontainers.containers.KafkaContainer
 import org.testcontainers.utility.DockerImageName
 
@@ -20,21 +18,19 @@ const val SAMPLE_TOPIC = "sample-topic"
 const val MESSAGES_COUNT = 100
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@kotlinx.coroutines.ExperimentalCoroutinesApi
 class Test {
 
-
-    private val log = LoggerFactory.getLogger(javaClass)
     private val container = KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:5.4.3"))
         .withEmbeddedZookeeper()
 
     @BeforeAll
     fun setup() {
         container.start()
-
     }
 
     @Test
-    fun test() = runBlocking<Unit> {
+    fun `should process all messages sequentially`() = runBlocking<Unit> {
 
         val producer = SampleProducer(bootstrapServers = container.bootstrapServers)
         val consumer = SampleConsumer(bootstrapServers = container.bootstrapServers)
@@ -47,7 +43,8 @@ class Test {
                 println("Processing message=$message. Please wait patiently.")
                 message.receiverOffset.commit().onErrorContinue { e, v ->
                     println("Error during commit of message=$message. Error=$e. value =$v")
-                }
+                }.awaitSingleOrNull()
+
                 println("Committed message=$message.")
                 delay(1000)
             }
